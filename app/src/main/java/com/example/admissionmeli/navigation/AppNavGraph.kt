4af62ddb0af4a4d11fd2c59762a4715c9d1ffdda
cristian <cristiankmil@gmail.com>
2023-10-14 22:44:50 -0500

    navigation app added

---
+++ app/src/main/java/com/example/admissionmeli/navigation/AppNavGraph.kt
@@ -0,0 +1,42 @@
+package com.example.admissionmeli.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.NavHostController
+import com.example.admissionmeli.core.navigation.Screens
+import com.example.admissionmeli.feature.item.navigation.itemDetailNavigation
+import com.example.admissionmeli.feature.search.navigation.searchNavigation
+import com.example.admissionmeli.dto.toParcelable
+
+
+@Composable
+fun AppNavGraph(
+    startDestination: String,
+    navController: NavHostController,
+) {
+    NavHost(
+        startDestination = startDestination,
+        navController = navController
+    ) {
+        searchNavigation(
+            onNavigateToItemDetail = { item ->
+                navController.currentBackStackEntry?.savedStateHandle?.set(
+                    Screens.Detail.ITEM_DETAIL_KEY,
+                    item.toParcelable()
+                )
+
+                navController.navigate(
+                    Screens.Detail.passItemId(
+                        productId = item.id
+                    )
+                )
+            })
+
+        itemDetailNavigation(
+            navController = navController,
+            onNavigateBack = {
+                navController.popBackStack()
+            }
+        )
+    }
+}
