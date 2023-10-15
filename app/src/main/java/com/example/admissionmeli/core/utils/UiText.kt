9911082f7b7bc32bc2cfcbf3db5b370d76cccf71
cristian <cristiankmil@gmail.com>
2023-10-14 21:49:33 -0500

    ui modified

--- app/src/main/java/com/example/admissionmeli/core/utils/UiText.kt
+++ app/src/main/java/com/example/admissionmeli/core/utils/UiText.kt
@@ -6,9 +6,18 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.res.stringResource
 
 sealed class UiText {
+    data class DynamicString(val value: String) : UiText()
     class StringResource(
         @StringRes val resId: Int,
         vararg val args: Any,
     ) : UiText()
+
+    @Composable
+    fun asString(): String {
+        return when (this) {
+            is DynamicString -> value
+            is StringResource -> stringResource(resId, *args)
+        }
+    }
 
 }
--- app/src/main/java/com/example/admissionmeli/ui/theme/Color.kt
+++ app/src/main/java/com/example/admissionmeli/ui/theme/Color.kt
@@ -5,17 +5,20 @@ import androidx.compose.material3.ColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 
+
 val Yellow80 = Color(0xFFFFC107)
-val YellowGrey80 = Color(0xFFFFEE58)
+val YellowGrey80 = Color(0xFFFAE204)
 val Amber80 = Color(0xFFFFECB3)
 
-val Yellow40 = Color(0xFFFFEE58)
-val YellowGrey40 = Color(0xFFFFF9C4)
+val Yellow40 = Color(0xFFFAE204)
+val YellowGrey40 = Color(0xFFFAE204)
 val Amber40 = Color(0xFFFFCA28)
 
+val md_theme_light_surface = Color(0xFFFFFBFF)
+val md_theme_dark_surfaceVariant = Color(0xFF49473A)
 
-val BrandColor = Color(0xFFFFF059)
-val NavBarColor = Color(0xFFF7D415)
+val BrandColor = Color(0xFFFAE204)
+val NavBarColor = Color(0xFFFAE204)
 val primaryButtonColor = Color(0xFF3782FC)
 val secondaryButtonColor = Color(0xFFE6ECfE)
 val onSecondaryButtonColor = Color(0xFF557ED0)
@@ -39,3 +42,7 @@ val ColorScheme.statusBarColor
 val ColorScheme.brandColor
     @Composable
     get() = if (!isSystemInDarkTheme()) BrandColor else BrandColor
+
+val ColorScheme.backgroundColor
+    @Composable
+    get() = if (!isSystemInDarkTheme()) md_theme_light_surface else md_theme_dark_surfaceVariant
