51a5fe1ef0860ff6d0bf659be28b11b27657ddc7
cristian <cristiankmil@gmail.com>
2023-10-14 21:53:15 -0500

    ui components core added

---
+++ app/src/main/java/com/example/admissionmeli/core/ui/components/IconText.kt
@@ -0,0 +1,47 @@
+package com.example.admissionmeli.core.ui.components
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.admissionmeli.R
+import com.example.admissionmeli.ui.theme.backgroundColor
+
+@Composable
+fun IconText(text:String, image:Int = R.drawable.error) {
+    Column(
+        modifier = Modifier
+            .background(MaterialTheme.colorScheme.backgroundColor)
+            .fillMaxSize()
+            .padding(16.dp)
+            .verticalScroll(rememberScrollState()),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally)
+    {
+        Image(
+            painter = painterResource(id = image),
+            contentDescription = "Image",
+            Modifier.size(40.dp)
+        )
+        Text(
+            text = text,
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Light,
+            color = MaterialTheme.colorScheme.onSurface
+        )
+    }
+}
